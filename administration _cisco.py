c15adef965b423c2cdfe73fcc614545ede57bf68
aspiownyou <56435240+aspiownyou@users.noreply.github.com>
2020-01-18 18:21:39 +0100

    Add files via upload

---
+++ administration _cisco.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# Importation des librairies nécessaires
+import os
+import subprocess
+# Le module os permet une interraction avec le système d'exploitation
+# le module subprocess permet l'exécution d'autre fichier Python à partir de celui-ci
+
+print("Configuration de matériels Cisco")
+
+menu_principal = """  
+\t1: Switch
+\t2: Routeur
+\t3: Fin du Programme
+"""
+
+choix = "0"
+while choix != "3":
+    choix = input(menu_principal)
+    if choix == "1": # Menu de la catégorie switch
+        switch = """
+        \t1: Configuration des vlans                       
+        \t2: Configuration des ports
+        \t3: Exit
+        """
+        choix_switch = "0"
+        while choix_switch != "3": # Execution des scripts 
+            choix_switch = input(switch)
+            if choix_switch == "1":
+                subprocess.call(["/usr/bin/python3", "/home/david/Documents/Python_Scripts/vlan.py"]) 
+            elif choix_switch == "2":
+                subprocess.call(["/usr/bin/python3", "/home/david/Documents/Python_Scripts/port_switch.py"])
+            else:
+                print("Retour au menu principal")
+            break
+            
+    elif choix == "2": # Menu de la catégorie router
+        router = """
+        \t1: Configuration du DHCP et du routage inter-vlan
+        \t2: Configuration du routage statique
+        \t3: Exit
+        """
+        choix_router = input(router)
+        while choix != "3": # execution des scripts
+            if choix_router == "1":
+                subprocess.call(["/usr/bin/python3", "/home/david/Documents/Python_Scripts/dhcp_routage_vlan.py"])
+            elif choix_router == "2":
+                subprocess.call(["/usr/bin/python3", "/home/david/Documents/Python_Scripts/"])
+            else:
+                print("Retour au menu principal")
+            break
+
+    else:
+        print("fin de programme")
